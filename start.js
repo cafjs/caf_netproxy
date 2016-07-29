f52a908ada87a2a2f4db6aad7e31c68ed6ebee44
Antonio Lain <antlai@cafjs.com>
2016-07-29 00:18:56 -0700

    Support eslint

--- start.js
+++ start.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
+'use strict';
 var np = require('./index.js');
 
 np.run([module], process.env['PROXY_JSON'], function(err, top) {
-           if (err) {
-               console.log('Error: ' + err);
-           } else {
-               console.log('Starting HAProxy manager...');
-               process.on('SIGTERM', function() {
-                   console.log("Caught SIGTERM signal (stop container)");
-                   top.__ca_graceful_shutdown__(null, function(err) {
-                       console.log('shutdown:' + (err ?  err : 'OK'));
-                   });
-               });
-           }
-       });
+    if (err) {
+        // eslint-disable-next-line
+        console.log('Error: ' + err);
+    } else {
+        // eslint-disable-next-line
+        console.log('Starting HAProxy manager...');
+        process.on('SIGTERM', function() {
+            // eslint-disable-next-line
+            console.log('Caught SIGTERM signal (stop container)');
+            top.__ca_graceful_shutdown__(null, function(err) {
+                // eslint-disable-next-line
+                console.log('shutdown:' + (err ? err : 'OK'));
+            });
+        });
+    }
+});
--- startLocal.js
+++ startLocal.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
+'use strict';
 var np = require('./index.js');
 
-np.run([module], 'proxy-local.json', function(err, top) {
-           if (err) {
-               console.log('Error: ' + err);
-           } else {
-               console.log('Starting HAProxy manager...');
-           }
-       });
+np.run([module], 'proxy-local.json', function(err) {
+    if (err) {
+        // eslint-disable-next-line
+        console.log('Error: ' + err);
+    } else {
+        // eslint-disable-next-line
+        console.log('Starting HAProxy manager...');
+    }
+});
